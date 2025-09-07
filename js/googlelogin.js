a691dbb3cf0f0f4b255154841834e4e3166f47d7
ObiloxYT <AfriaStudios@outlook.in>
2025-09-07 07:21:17 +0000

    profile data

--- js/googlelogin.js
+++ js/googlelogin.js
@@ -252,3 +252,4 @@ export async function updateProfile({ display_name, profile_picture, bio }) {
   if (error) return { error: error.message };
   return { success: true, data };
 }
+console.log("profile data:",getProfile())
